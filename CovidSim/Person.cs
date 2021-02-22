63473049e3297560a37910e991f6d2226a590e9f
Lukas Josai <lukas.josai@student.tuke.sk>
2021-02-22 11:29:54 +0100

    Enums

--- CovidSim/Person.cs
+++ CovidSim/Person.cs
@@ -4,7 +4,40 @@ using System.Text;
 
 namespace CovidSim
 {
-    class Person
+    public class Person
     {
+        public HealthStatus Health { get; set; }
+
+        public void meet(Person otherPerson)
+        {
+
+        }
+
+        public bool IsContagious => Health != HealthStatus.Healthy || Health != HealthStatus.Survived || Health != HealthStatus.Deceased;
+
+        public enum HealthStatus
+        {
+            Healthy,
+            Asymptomatic,
+            Symptoms,
+            Infected,
+            Survived,
+            Deceased
+        }
+
+        public enum QuarantineStatus
+        {
+            Normal,
+            HomeQuarantine,
+            Hospital
+        }
+
+        public enum CitizenClass
+        {
+            Citizen,
+            MedicalStaff,
+            Military,
+            FirstResponder
+        }
     }
 }
