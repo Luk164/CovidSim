b0458bbb86fcb43fe821e3f26546d6d477cbdd46
TonyStarj <jakub.vargaestok@student.tuke.sk>
2021-02-24 09:03:20 +0100

    Auto stash before rebase of "origin/Lukas"

---
+++ NUnitTests/GearEntitiesTests/MaskTest.cs
@@ -0,0 +1,34 @@
+﻿using CovidSim.GearClasses;
+using NUnit.Framework;
+
+namespace NUnitTests.GearEntitiesTests
+{
+    [TestFixture]
+    class MaskTest
+    {
+        private Mask mask;
+
+        //TODO - set via reference?
+        private short _protectionModifierValue = 20;
+        private short _preventionModifierValue = 95;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            mask = new Mask();
+        }
+
+        [Test]
+        public void ProtectionModifierTest()
+        {
+            Assert.AreEqual(_protectionModifierValue, mask.ProtectionModifier.Value);
+        }
+
+        [Test]
+        public void PreventionModifierTest()
+        {
+            Assert.AreEqual(_preventionModifierValue, mask.PreventionModifier.Value);
+        }
+    }
+}
---
+++ NUnitTests/ModifierTest.cs
@@ -0,0 +1,32 @@
+﻿using CovidSim;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    class ModifierTest
+    {
+        [TestCase(0, TestName = "Modifier.set = 0", ExpectedResult = 0)]
+        [TestCase(20, TestName = "Modifier.set = 20", ExpectedResult = 20)]
+        [TestCase(50, TestName = "Modifier.set = 50", ExpectedResult = 50)]
+        [TestCase(100, TestName = "Modifier.set = 100", ExpectedResult = 100)]
+        public short ModifierSetTest(short value)
+        {
+            return new Modifier { Value = value }.Value;
+        }
+
+        [TestCase(-1, TestName = "Modifier.set = -1", Ignore = "ArgumentOutOfRangeException")]
+        [TestCase(101, TestName = "Modifier.set = 101", Ignore = "ArgumentOutOfRangeException")]
+        public void ModifierOutOfRangeTest(short value)
+        {
+            Modifier mod = new Modifier {};
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => mod.Value = value);
+            Assert.That(ex.Message, Is.EqualTo("Specified argument was out of the range of valid values."));
+        }
+    }
+}
