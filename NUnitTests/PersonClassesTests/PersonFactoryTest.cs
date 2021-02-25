23ae0ea9821b94900d94489978879c01899d9980
TonyStark <jakub.vargaestok@student.tuke.sk>
2021-02-25 06:06:38 +0100

    testing

---
+++ NUnitTests/PersonClassesTests/PersonFactoryTest.cs
@@ -0,0 +1,24 @@
+﻿using CovidSim.PersonClasses;
+using NUnit.Framework;
+using System;
+
+namespace NUnitTests.PersonClassesTests
+{
+    [TestFixture]
+    class PersonFactoryTest
+    {
+        private Person personCitizen = PersonFactory.GenerateCitizen();
+        private Person personMedicalStaff = PersonFactory.GenerateMedicalStaff();
+        private Person personMilitary = PersonFactory.GenerateMedicalStaff();
+        private Person personFirstResponder = PersonFactory.GenerateFirstResponder();
+
+        [Test]
+        public void MakePerson()
+        {
+            Assert.IsNotNull(personCitizen);
+            Assert.IsNotNull(personMedicalStaff);
+            Assert.IsNotNull(personMilitary);
+            Assert.IsNotNull(personFirstResponder);
+        }
+    }
+}
