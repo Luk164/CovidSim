79f055dda82661a4bb474bfe2d8b1122c35d06e6
TonyStark <jakub.vargaestok@student.tuke.sk>
2021-02-26 14:22:25 +0100

    PersonTest - OK, PersonFactory - OK

--- NUnitTests/PersonClassesTests/PersonFactoryTest.cs
+++ NUnitTests/PersonClassesTests/PersonFactoryTest.cs
@@ -7,18 +7,53 @@ namespace NUnitTests.PersonClassesTests
     [TestFixture]
     class PersonFactoryTest
     {
-        private Person personCitizen = PersonFactory.GenerateCitizen();
-        private Person personMedicalStaff = PersonFactory.GenerateMedicalStaff();
-        private Person personMilitary = PersonFactory.GenerateMedicalStaff();
-        private Person personFirstResponder = PersonFactory.GenerateFirstResponder();
+        private Person personCitizen;
+        private Person personMedicalStaff;
+        private Person personMilitary;
+        private Person personFirstResponder;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            personCitizen = PersonFactory.GenerateCitizen();
+            personMedicalStaff = PersonFactory.GenerateMedicalStaff();
+            personMilitary = PersonFactory.GenerateMilitary();
+            personFirstResponder = PersonFactory.GenerateFirstResponder();
+        }
 
         [Test]
-        public void MakePerson()
+        public void MakePersonTest()
         {
             Assert.IsNotNull(personCitizen);
+            Assert.AreEqual(Person.CitizenClassEnum.Citizen, personCitizen.CitizenClass);
+            
             Assert.IsNotNull(personMedicalStaff);
+            Assert.AreEqual(Person.CitizenClassEnum.MedicalStaff, personMedicalStaff.CitizenClass);
+
             Assert.IsNotNull(personMilitary);
+            Assert.AreEqual(Person.CitizenClassEnum.Military, personMilitary.CitizenClass);
+
             Assert.IsNotNull(personFirstResponder);
+            Assert.AreEqual(Person.CitizenClassEnum.FirstResponder, personFirstResponder.CitizenClass);
+        }
+
+        [Test]
+        public void SetOptionsAndGearToPersonTest()
+        {
+            Assert.IsNotNull(personCitizen.Health);
+            Assert.AreEqual(personCitizen.Gear.Count, 0);
+
+            Assert.AreEqual(90, personMedicalStaff.Options.GearCompliance.Value);
+            Assert.AreEqual(100, personMedicalStaff.Options.QuarantineCompliance.Value);
+            Assert.AreEqual(4, personMedicalStaff.Gear.Count);
+
+            Assert.AreEqual(90, personMilitary.Options.GearCompliance.Value);
+            Assert.AreEqual(100, personMilitary.Options.QuarantineCompliance.Value);
+            Assert.AreEqual(3, personMilitary.Gear.Count);
+
+            Assert.AreEqual(90, personFirstResponder.Options.GearCompliance.Value);
+            Assert.AreEqual(100, personFirstResponder.Options.QuarantineCompliance.Value);
+            Assert.AreEqual(4, personFirstResponder.Gear.Count);
         }
     }
 }
--- NUnitTests/PersonClassesTests/PersonTest.cs
+++ NUnitTests/PersonClassesTests/PersonTest.cs
@@ -8,17 +8,53 @@ namespace NUnitTests.PersonClassesTests
     [TestFixture]
     class PersonTest
     {
-        private Person personCitizen;
-        private Person personMedicalStaff;
-        private Person personMilitary;
-        private Person personFirstResponder;
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
+        private Person person;
+
+        [SetUp]
+        public void Setup()
         {
-            personCitizen = PersonFactory.GenerateCitizen();
-            personMedicalStaff = PersonFactory.GenerateMedicalStaff();
-            personMilitary = PersonFactory.GenerateMedicalStaff();
-            personFirstResponder = PersonFactory.GenerateFirstResponder();
+            person = PersonFactory.GenerateCitizen();
+        }
+
+        [TestCase(Person.HealthStatusEnum.Asymptomatic, ExpectedResult = true)]
+        [TestCase(Person.HealthStatusEnum.Deceased, ExpectedResult = false)]
+        [TestCase(Person.HealthStatusEnum.Healthy, ExpectedResult = false)]
+        [TestCase(Person.HealthStatusEnum.Immune, ExpectedResult = false)]
+        [TestCase(Person.HealthStatusEnum.SeriouslyIll, ExpectedResult = true)]
+        [TestCase(Person.HealthStatusEnum.Symptoms, ExpectedResult = true)]
+        public bool IsContagiousTest(Person.HealthStatusEnum healthStatus)
+        {
+            return new Person {Health = healthStatus}.IsContagious;
+        }
+
+        [Test]
+        public void PreventionTest()
+        {
+            Assert.AreEqual(person.Prevention, 0);
+            Assert.GreaterOrEqual(PersonFactory.GenerateMedicalStaff().Prevention, 99);
+            Assert.GreaterOrEqual(PersonFactory.GenerateMilitary().Prevention, 99);
+            Assert.GreaterOrEqual(PersonFactory.GenerateFirstResponder().Prevention, 99);
+        }
+
+        [Test]
+        public void ProtectionTest()
+        {
+            Assert.AreEqual(person.Prevention, 0);
+            Assert.GreaterOrEqual(PersonFactory.GenerateMedicalStaff().Protection, 97.70);
+            Assert.GreaterOrEqual(PersonFactory.GenerateMilitary().Protection, 88.79);
+            Assert.GreaterOrEqual(PersonFactory.GenerateFirstResponder().Protection, 97.70);
+        }
+
+        [TestCase(Person.HealthStatusEnum.Healthy, Person.HealthStatusEnum.Asymptomatic, ExpectedResult = true)]
+        [TestCase(Person.HealthStatusEnum.Asymptomatic, Person.HealthStatusEnum.Healthy, ExpectedResult = false)]
+        public bool MeetTest(Person.HealthStatusEnum meetPersonHealth, Person.HealthStatusEnum personHealth)
+        {
+            Person meetPerson = PersonFactory.GenerateMedicalStaff();
+            meetPerson.Health = meetPersonHealth;
+            person.Health = personHealth;
+
+            person.Meet(meetPerson);
+            return person.IsContagious;
         }
     }
 }
