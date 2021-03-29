c87b70002456b64db80f69845b2197e6220351e0
Lukas Josai <lukas.josai@student.tuke.sk>
2021-03-29 11:41:44 +0200

    Cleanup

--- CovidSim/MeetList.cs
+++ CovidSim/MeetList.cs
@@ -6,6 +6,6 @@ namespace CovidSim
     public class MeetList
     {
         public Person Person { get; set; }
-        public List<Person> PeopleToMeet { get; set; } = new List<Person>();
+        public List<Person> PeopleToMeet { get; set; } = new();
     }
 }
